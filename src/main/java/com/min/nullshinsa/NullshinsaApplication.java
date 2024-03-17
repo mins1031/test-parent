f968b7ad33cadf1f89b314edf01a8aa54a0a4d71
mins1031 <urisegea@naver.com>
2024-03-17 19:10:11 +0900

    최초 커밋

---
+++ src/main/java/com/min/nullshinsa/NullshinsaApplication.java
@@ -0,0 +1,13 @@
+package com.min.nullshinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullshinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullshinsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/min/nullshinsa/NullshinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.nullshinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullshinsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
